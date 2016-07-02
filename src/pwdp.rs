2aa023c25a37697dbc3c4d7cd41790a2909124e4
Ed Lewis <edward.richard.lewis@gmail.com>
2016-07-02 19:26:19 +0100

    WIP

--- src/pwdp.rs
+++ src/pwdp.rs
@@ -1,10 +1,31 @@
 use persist::*;
 
-pub fn search(search_for:String, store:Store) -> Vec<Entry> {
+pub fn search<'a>(search_for:&str, store:&'a Store) -> Vec<&'a Entry> {
     return vec![];
 }
 
 #[test]
-fn test_store() {
-    assert_eq!("hello", "world");
+fn test_store_search_title() {
+    let hit = Entry { 
+            title: "abc hit abc".to_string(), 
+            username: "user".to_string(),
+            password: "password".to_string(),
+            link: "link".to_string(),
+            additional: "additional".to_string() 
+        };  
+
+    let miss =  Entry { 
+            title: "abc something".to_string(), 
+            username: "user".to_string(),
+            password: "password".to_string(),
+            link: "link".to_string(),
+            additional: "additional".to_string() 
+        }; 
+
+    let mut store = Store::new();
+    store.add(hit);
+    store.add(miss);
+    let hits = search("hit", &store);
+    assert_eq!(hits.len(), 1);
+    assert_eq!(*hits[0], hit); 
 }
